cf5ae40217f56aa849dd20aaf56c70e0d03ced3a
dskoo416 <61358001+dskoo416@users.noreply.github.com>
2024-03-21 10:40:28 -0400

    login done

--- FitUpBack/login/server.js
+++ FitUpBack/login/server.js
@@ -1,15 +1,46 @@
-const express = require ('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
 
-const users = []
+app.use(express.json());
 
-app.get('/users', (req,res) => {
-    res.json(users)
-})
+//Save this to sql later
+const users = [];
 
-app.post('/users', (req, res) => {
-    const user = {name: req.body.name, password: req.body.password}
-    users.push(user)
-})
+app.get('/users', (req, res) => {
+    res.json(users);
+});
 
-app.listen(3000)
+
+//Registering User
+app.post('/users', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = { email: req.body.email, password: hashedPassword };
+        users.push(user);
+        res.status(201).send();
+    } catch {
+        res.status(500).send();
+    }
+});
+
+
+//Login
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.email === req.body.email);
+    if (user == null) {
+        //Use the same message for user not found for bette security
+        return res.status(400).send('Login Failed'); 
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success');
+        } else {
+            res.send('Login Failed');
+        }
+    } catch {
+        res.status(500).send();
+    }
+});
+
+app.listen(3000);
