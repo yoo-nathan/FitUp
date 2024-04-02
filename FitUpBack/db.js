e21d0e8618205c769132c7458eae1434caa4eb0a
dskoo416 <61358001+dskoo416@users.noreply.github.com>
2024-04-02 16:04:24 -0400

    jebal

--- FitUpBack/db.js
+++ FitUpBack/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: '35.196.58.227',
-    user: 'root',
+    user: 'daniel',
     database: 'user', 
     password: '1q2w3e4r!Q@W#E$R!',
 });
