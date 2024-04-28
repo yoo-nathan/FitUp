64ebef4ab99e214b28016d431d008c1a669c779f
dskoo416 <61358001+dskoo416@users.noreply.github.com>
2024-04-28 01:27:10 -0400

    profile screen picture fetch

--- FitUpFront/navigation/screens/ProfileScreen.jsx
+++ FitUpFront/navigation/screens/ProfileScreen.jsx
@@ -8,37 +8,80 @@ import {
   Image,
   TouchableOpacity,
   Dimensions
- } from 'react-native';
+} from 'react-native';
 import { getFirstName, getUserEmail } from '../../service/getService';
 import { getMyID } from '../../service/chatService';
 
 const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
 
+
+
+
+
 export default function ProfileScreen({ navigation }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  
 
-  useEffect(() => {
-    const fetchInfo = async () => {
-      const token = await AsyncStorage.getItem('userToken');
-      const uid = await getMyID(token);
-      const userName = await getFirstName(uid);
-      setName(userName);
+  //edit 1
+  const [userImage, setUserImage] = useState(null);
 
-      const userEmail = await getUserEmail(uid);
-      setEmail(userEmail.email);
+  const fetchUserImage = async () => {
+    const token = await AsyncStorage.getItem('userToken');
+    const uid = await getMyID(token);
+  
+    try {
+      const response = await fetch("https://cs-370-420520.ue.r.appspot.com/getInfo/getPic", {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          UID: uid,
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP status ${response.status}`);
+      }
+      const imageBlob = await response.blob();
+      const reader = new FileReader();
+      reader.onload = () => {
+        setUserImage(reader.result);
+        console.log('Image set successfully');
+      };
+      reader.onerror = (error) => {
+        console.error('Error reading blob', error);
+      };
+      reader.readAsDataURL(imageBlob);
+    } catch (error) {
+      console.error('There was an error fetching the image:', error);
     }
-    fetchInfo();
-  }, [])
+  };
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+  const fetchInfo = async () => {
+    const token = await AsyncStorage.getItem('userToken');
+    const uid = await getMyID(token);
+    const userName = await getFirstName(uid);
+    setName(userName);
+
+    const userEmail = await getUserEmail(uid);
+    setEmail(userEmail.email);
+  }
+  fetchInfo();
+  fetchUserImage();
+}, []) 
 
   return (
     <View style={styles.mainContainer}>
       <View style={{alignItems:'center', justifyContent:'center', height: 250}}>
-        <Image resizeMode='contain'
-            style={styles.iconImg}
-            source={{uri:'https://cdn2.iconfinder.com/data/icons/colored-simple-circle-volume-01/128/circle-flat-general-51851bd79-512.png'}}/>
-        <Text style={styles.headerText}>{name}</Text>
+      <Image 
+  resizeMode='contain'
+  style={styles.iconImg}
+  source={userImage ? { uri: userImage } : { uri: 'https://res.cloudinary.com/peloton-cycle/image/fetch/f_auto,c_limit,w_3840,q_90/https://images.ctfassets.net/6ilvqec50fal/7phXLCGAsmdelHmGrb33ID/1407d5437076e04de863901ad121eb52/talk-test-conversational-pace.jpg' }}
+/>
+
+
         <Text style={styles.emailText}>{email}</Text>
       </View>
       <View style={styles.lowerView}>
@@ -46,9 +89,10 @@ export default function ProfileScreen({ navigation }) {
         onPress={() => navigation.navigate('EditProfile')}
         >
           <View style={{flexDirection: 'row'}}>
-            <Image resizeMode='contain'
+          <Image resizeMode='contain'
               style={styles.widgetImg}
               source={{uri:'https://static-00.iconduck.com/assets.00/edit-profile-icon-512x421-6kngp5gu.png'}}/>
+
             <Text style={styles.widgetText}> Edit Profile</Text>
           </View>
           <Text style={styles.arrowText}>&#187;</Text>
@@ -62,6 +106,7 @@ export default function ProfileScreen({ navigation }) {
           <View style={{flexDirection: 'row'}}>
             <Image resizeMode='contain'
               style={styles.widgetImg}
+              //Question Mark
               source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrG5wPpOIB74fs9jyiHy19EAcyS3XLfaNgR5p5rzDWMA&s'}}/>
             <Text style={styles.widgetText}> Contact FitUp </Text>
           </View>
@@ -88,6 +133,7 @@ export default function ProfileScreen({ navigation }) {
           <View style={{flexDirection: 'row'}}>
             <Image resizeMode='contain'
               style={styles.widgetImg}
+              //Logout Logo
               source={{uri: 'https://i.pinimg.com/564x/b8/c9/9e/b8c99e1eb63169d5a862bbf90b171d28.jpg'}}/>
             <Text style={styles.widgetText}> Logout </Text>
           </View>
@@ -102,6 +148,8 @@ export default function ProfileScreen({ navigation }) {
           <View style={{flexDirection: 'row'}}>
             <Image resizeMode='contain'
               style={styles.widgetImg}
+
+              //Trash
               source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkEqb7yH6GMN43ZOyS5_LiZvWutK3h5ihP1Q93v7T6qA&s'}}/>
             <Text style={styles.widgetText}> Delete Account</Text>
           </View>
